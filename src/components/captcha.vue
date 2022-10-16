8964fa343525cfa28bb362129d9bc7b00e1b9324
丁心宜 <1363867975@qq.com>
2022-10-16 09:53:51 +0800

    监听,改善性能

--- src/components/captcha.vue
+++ src/components/captcha.vue
@@ -4,19 +4,24 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch } from "vue";
 import api from "@/axios";
 const checkCode = ref("");
 const svg = ref(null);
 const str = ref("");
+const is_equal = ref(false);
 const emit = defineEmits(["getData"]);
 
 onMounted(() => {
   refresh();
 });
 const check = () => {
-  emit("getData", checkCode.value === str.value);
+  is_equal.value = checkCode.value === str.value;
 };
+//监听,只有状态改变才emit
+watch(is_equal, (val) => {
+  emit("getData", val);
+});
 const refresh = async () => {
   try {
     const data = await api.captcha();
