1501b059259c7d16c68b66ce832c1b609d106813
丁心宜 <1363867975@qq.com>
2022-10-18 09:08:50 +0800

    分离pop over组件

---
+++ src/components/popover.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-popover trigger="hover" placement="bottom" popper-class="infoPopover">
+    <template #reference>
+      <img
+        :src="
+          userInfo.getUserInfo.avatar
+            ? `${userInfo.getUserInfo.avatar}`
+            : '../../public/imgs/peach.svg'
+        "
+        alt=""
+        class="avatar"
+      />
+    </template>
+    <template class="popoverList" #default>
+      <router-link :to="{ name: 'Userpage' }" class="popoverItem"
+        >我的主页</router-link
+      >
+      <router-link :to="{ name: 'MyInfo' }" class="popoverItem"
+        >个人中心</router-link
+      >
+      <div class="popoverItem" @click="logout">退出登录</div>
+    </template>
+  </el-popover>
+</template>
+
+<script setup>
+import {} from "vue";
+import { useInfoStore } from "@/store";
+import router from "@/router";
+const userInfo = useInfoStore();
+
+
+
+const logout = () => {
+    router.push({ name: "Login" });
+  userInfo.$reset();
+ 
+};
+</script>
+
--- src/pages/mainPage/TabBar.vue
+++ src/pages/mainPage/TabBar.vue
@@ -4,49 +4,18 @@
     <span :class="$style.title">CourseTable </span>
     <span :class="$style.ch">课程管理系统</span>
     <div :class="$style.middle"></div>
-    <span :class="$style.username"> {{ userInfo.getUserInfo.username }} , 你好!</span>
-    <el-popover trigger="click" placement="bottom" popper-class="infoPopover">
-      <template #reference>
-        <img
-          :src="
-            userInfo.getUserInfo.avatar ? `${userInfo.getUserInfo.avatar}` : '../../../imgs/peach.svg'
-          "
-          alt=""
-          :class="$style.avatar"
-        />
-      </template>
-      <template class="popoverList" #default>
-        <router-link :to="{ name: 'Userpage' }" class="popoverItem"
-          >我的主页</router-link
-        >
-        <router-link :to="{ name: 'MyInfo' }" class="popoverItem"
-          >个人中心</router-link
-        >
-        <div class="popoverItem" @click="logout">退出登录</div>
-      </template>
-    </el-popover>
+    <span :class="$style.username">
+      {{ userInfo.getUserInfo.username }} , 你好!</span
+    >
+    <Popover />
   </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
-
 import { useInfoStore } from "@/store";
-import router from "../../router";
-
+import Popover from "@/components/popover.vue";
 
 const userInfo = useInfoStore();
-
-const logout = () => {
-  userInfo.$reset();
-  router.push({ name: "Login" });
-};
-
-onMounted(async () => {
-  console.log(userInfo.getUserInfo);
-  
-
-});
 </script>
 
 <style module lang="less">
@@ -91,14 +60,6 @@ onMounted(async () => {
     padding-right: 2rem;
     vertical-align: middle;
   }
-
-  .avatar {
-    flex: 0 0 auto;
-    order: 5;
-    width: 2.7rem;
-    cursor: pointer;
-    vertical-align: middle;
-  }
 }
 </style>
 <style lang="less">
@@ -106,33 +67,40 @@ onMounted(async () => {
   font-size: 2rem;
   padding: 0;
   --el-popover-padding: 0;
-  .popoverList {
-    display: flex;
-    flex-direction: column;
+}
+.avatar {
+  flex: 0 0 auto;
+  order: 5;
+  width: 2.7rem;
+  cursor: pointer;
+  vertical-align: middle;
+}
+.popoverList {
+  display: flex;
+  flex-direction: column;
+}
+.popoverItem {
+  text-align: center !important;
+  padding: 0.5rem 0 1rem 0;
+  cursor: pointer;
+  color: #796bf1;
+
+  text-decoration: none;
+  transition: all 0.3s;
+
+  &:first-child {
+    display: block;
+    padding: 1rem 0 0.5rem 0;
   }
-  .popoverItem {
-    text-align: center !important;
-    padding: 0.5rem 0 1rem 0;
-    cursor: pointer;
-    color: #796bf1;
+  &:nth-of-type(2) {
+    padding: 0.5rem 0;
+    display: block;
+  }
 
-    text-decoration: none;
+  &:hover {
+    background-color: #796bf1;
+    color: #fff;
     transition: all 0.3s;
-
-    &:first-child {
-      display: block;
-      padding: 1rem 0 0.5rem 0;
-    }
-    &:nth-of-type(2) {
-      padding: 0.5rem 0;
-      display: block;
-    }
-
-    &:hover {
-      background-color: #796bf1;
-      color: #fff;
-      transition: all 0.3s;
-    }
   }
 }
 </style>
