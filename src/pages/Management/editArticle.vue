60f18a7d31f3411a63fa2f6c760ba324ab315bd8
丁心宜 <1363867975@qq.com>
2022-10-23 04:50:16 +0800

    编辑文章界面完善

--- src/pages/Management/editArticle.vue
+++ src/pages/Management/editArticle.vue
@@ -5,17 +5,25 @@
       @click="router.back(-1)"
       :class="$style.leftIcon"
     ></i>
+    <input type="text" :class="$style.title" />
+    <button v-if="props.mid" :class="$style.submitBtn">修改文章</button>
+    <button v-else :class="$style.submitBtn">发布文章</button>
+  </div>
+  <div :class="$style.editorContent">
+    <MdEditor :class="$style.editor" />
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
+import MdEditor from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
 
 const props = defineProps(["mid"]);
 
 const router = useRouter();
-// const mid = ref(route.params.mid);
+
 onMounted(() => {
   console.log(props.mid);
 });
@@ -23,7 +31,7 @@ onMounted(() => {
 
 <style module lang="less">
 .top {
-  background: url("https://s2.loli.net/2022/10/22/ptmb9gEkLQfOl7B.png") no-repeat;
+  background: #d3a2f1;
   position: fixed;
   top: 0;
   width: 100%;
@@ -32,12 +40,56 @@ onMounted(() => {
   box-sizing: border-box;
   line-height: 3.5rem;
   display: flex;
-
+  align-items: center;
   color: #fff;
-
+  z-index: 999;
   .leftIcon {
     font-size: 2rem;
     cursor: pointer;
+    flex: 0 0;
+  }
+  .title {
+    flex: 1 1;
+    height: 2.5rem;
+    border-radius: 2rem;
+    border: 0;
+    margin: 0 1rem;
+    transition: all 0.2s;
+    padding: 0 1.5rem;
+    font-size: 1.1rem;
+    &:focus-visible {
+      outline: 2px solid #9025d2 !important;
+      transition: all 0.2s;
+      box-shadow: 0 0 5px #9025d2;
+    }
+  }
+  .submitBtn {
+    letter-spacing: 1px;
+    display: inline-block;
+    width: 8rem;
+    height: 2.8rem;
+    font-size: 1.1rem;
+    border: 0;
+    background-color: #9025d2;
+    color: #fff;
+    border-radius: 2rem;
+    transition: all 0.2s;
+    &:hover {
+      border: 2px solid #9025d2;
+      color: #9025d2;
+      background: #fff;
+      transition: all 0.2s;
+    }
+  }
+}
+.editorContent {
+  position: relative;
+  padding-top: 3.5rem;
+  width: 100%;
+  height: 100vh;
+  box-sizing: border-box;
+  .editor {
+    height: 100%;
   }
 }
 </style>
